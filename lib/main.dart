31c0594608ce78b2ed0432ec21ad1dab2f569a26
Anik7303 <anik7303@gmail.com>
2021-05-03 15:50:04 +0600

    using ChangeNotifierProvider with ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Person with ChangeNotifier {
   final String name;
@@ -13,13 +14,46 @@ class Person with ChangeNotifier {
 }
 
 void main() {
-  Person person = Person(name: 'Yohan', age: 25);
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => Person(name: 'Yohan', age: 25),
+      child: MyApp(),
+    ),
+  );
+}
 
-  // 'addListener' is a method on the 'ChangeNotifier' class,
-  // which is mixed-in to the Person class
-  person.addListener(() {
-    print('value updated: ${person.age}');
-  });
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
 
-  person.increaseAge();
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider Class'),
+      ),
+      body: Center(
+        child: Text(
+          'Hi ${Provider.of<Person>(context).name}!\nYou are ${Provider.of<Person>(context).age} years old.',
+          textAlign: TextAlign.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          size: 30.0,
+        ),
+        onPressed: () =>
+            Provider.of<Person>(context, listen: false).increaseAge(),
+      ),
+    );
+  }
 }
